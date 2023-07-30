ea39f789e218af187fc2b76bf6f8dafe147a3ac7
Kirill Kutovuy <114417403+Kirill99k@users.noreply.github.com>
2023-07-31 00:22:07 +0300

    Add theme

---
+++ src/components/constants/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    white: '#ffffff',
+    black: '#000000',
+    background: '#1b9fa8',
+    backdrop: 'rgba(0, 0, 0, 0.8)',
+    headerBg: '#1b9fa8',
+    textShadow: 'rgba(255, 255, 255, 0.5)',
+    shadow: 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
+  },
+};
