6d13476fd6e6f2f46fbc42ef9e6641c989df77b1
Kirill Kutovuy <114417403+Kirill99k@users.noreply.github.com>
2023-07-31 00:23:54 +0300

    Adds ImageGalleryItem

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,33 @@
+import { Modal } from 'components/Modal';
+import { Component } from 'react';
+import { GalleryItem, Image } from './ImageGalleryItem.styled';
+
+export class ImageGalleryItem extends Component {
+  state = {
+    showModal: false,
+  };
+
+  openModal = () => {
+    this.setState({ showModal: true });
+  };
+  closeModal = () => {
+    this.setState({ showModal: false });
+  };
+
+  render() {
+    const { image } = this.props;
+    const { showModal } = this.state;
+    return (
+      <>
+        <GalleryItem onClick={this.openModal}>
+          <Image src={image.webformatURL} alt={image.tags}></Image>
+        </GalleryItem>
+        {showModal && (
+          <Modal onClose={this.closeModal}>
+            <img src={image.largeImageURL} alt={image.tags} />
+          </Modal>
+        )}
+      </>
+    );
+  }
+}
